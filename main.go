4874c540f5555003862f83492a9b28ee648e5b53
Akyl <94756078+Alamansson@users.noreply.github.com>
2022-04-27 03:24:52 +0600

    gitignore .env

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
+// https://tproger.ru/translations/deploy-a-secure-golang-rest-api/
+// https://medium.com/@adigunhammedolalekan/build-and-deploy-a-secure-rest-api-with-go-postgresql-jwt-and-gorm-6fadf3da505b
+
 import (
 	"fmt"
 	"github.com/alamansson/contact-api/app"
